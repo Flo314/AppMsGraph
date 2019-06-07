05ba1a0c7d29fe94f2e47398be71d2f7f70ea7d0
“Florent <f.losada@hotmail.fr>
2019-06-07 10:21:54 +0200

    test control formulaire ok pour comment et date

--- app/src/main/java/com/example/appmsgraph/utils/Validator.java
+++ app/src/main/java/com/example/appmsgraph/utils/Validator.java
@@ -20,7 +20,7 @@ public class Validator {
 
     public static Boolean validatorDate(String date) {
         boolean valid = true;
-        if (date != null && !date.isEmpty()) {
+        if (date.equals("")|| date.equals("Date")) {
             Pattern pattern = Pattern.compile("^(0[1-9]|[12][0-9]|[3][01])/(0[1-9]|1[012])/\\\\d{4}$");
             Matcher matcher = pattern.matcher(date);
             if (!matcher.find()) {
